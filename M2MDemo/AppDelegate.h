c4b60c75d7b8319b233d609a9dcfd7096331ee8a
ishida <ishida@ishida-no-iMac.local>
2014-10-13 10:50:56 +0900

    Initial Commit

---
+++ M2MDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  M2MDemo
+//
+//  Created by ishida on 2014/10/13.
+//  Copyright (c) 2014年 ishida. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ M2MDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  M2MDemo
+//
+//  Created by ishida on 2014/10/13.
+//  Copyright (c) 2014年 ishida. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
